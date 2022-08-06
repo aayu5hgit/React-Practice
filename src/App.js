1af6702b5dcb62dff1a5dc804ad85f226d6801c8
AAYUSH TALREJA <86314754+aayu5hgit@users.noreply.github.com>
2022-08-07 00:21:17 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Footer from './Components/Footer';
+
+function App() {
+  const productList = [
+    {
+      name: "IPhone 11",
+      price: 49999,
+      quantity: 0,
+    },
+    {
+      name: "IPhone 12",
+      price: 56000,
+      quantity: 0,
+    },
+    {
+      name: "IPhone 12 Pro",
+      price: 66000,
+      quantity: 0,
+    },
+
+  ]
+  return (
+   <>
+   <Navbar/>
+   <main className='container mt-5 text-left'>
+   <ProductList productList={productList}/>
+   </main>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Product from './Product'
+
+export default function ProductList(props) {
+  return (
+    <div className='row justify-content-center'>
+        <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card m-4" style={{width: '19rem'}}>
+        <img src="https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">IPhone 11</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary m-2">Add To Cart</a>
+          <a href="#" className="btn btn-primary m-2">Buy Now</a>
+        </div>
+      </div>
+    </div>
+    
+    
+
+      
+    // props.productList.map((product, i)=>{
+    //     return <Product product={product} key={i}/>
+    // })
+  )
+  
+}
